a416c294ced82ff79a17c80d02fd06fefb3f0b4b
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2024-06-15 09:49:11 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Auto Store</p>
+    </footer>
+  );
+}
+
+export default Footer;
